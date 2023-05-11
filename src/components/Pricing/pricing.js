4e71e741a3e1a083ea83c2e4025f87f7b458e35f
keerthinanjundareddy <keerthana@apprikart.com>
2023-05-11 11:03:47 +0530

    space

--- src/components/Pricing/pricing.js
+++ src/components/Pricing/pricing.js
@@ -26,13 +26,13 @@ const responsive = {
     breakpoint: { max: 1024, min: 464 },
     items: 1,
     slidesToSlide: 1,
-  //   itemPadding: 20, // optional, default to 1.
+    // itemPadding: 20, // optional, default to 1.
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
     items: 1,
     slidesToSlide: 1,
-  //   itemPadding: 20, // optional, default to 1.
+    itemPadding: 20, // optional, default to 1.
   },
 };
 
